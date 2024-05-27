b1fbe2d815f1acc4c30a98ec92062876ec0801dd
John Wain <tolikeamoveit14@gmail.com>
2024-05-27 17:55:01 +0300

    Apply commandline help,2files,format

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,27 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
 
+import java.util.concurrent.Callable;
+@Command(name = "gendiff", version = "Groovy picocli v4.0 demo",
+        mixinStandardHelpOptions = true, // add --help and --version options
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+    @CommandLine.Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+    @CommandLine.Parameters(index = "0", description = "path to second file")
+    private String filepath2;
+    @CommandLine.Option(names = {"-f", "--format"}, defaultValue = "stylish", description = "output format [default: stylish]")
+    private String format = "stylish";
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("Hello World!");
+
+        return 0;
+    }
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
 }
